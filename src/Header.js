fb6e9a9906d4f4558b99d7d60203a068c395e9f8
Pepista <pcechak07@gmail.com>
2024-11-25 19:22:45 +0100

    Juhů

--- src/Header.js
+++ src/Header.js
@@ -1,23 +1,21 @@
-    import './Header.css'; // Import stylů
-    import { Link } from 'react-router-dom'; // Import React Routeru
+import './Header.css'; // Import stylů
+import { Link } from 'react-router-dom'; // Import React Routeru
 
-    const Header = () => {
-        return (
-            <header className="header">
-                <div className="container">
-                    <h1>Pedro Joestar</h1>
-                    <nav className="nav">
-            <ul className="nav-links">
-            
-            <li><Link to="/">Domů</Link></li>
-            <li><Link to="/About">O mně</Link></li>
-            <li><Link to="/Projekty">Projekty</Link></li>
-            
-            </ul>
-        </nav>
-                </div>
-            </header>
-        );
-    }
+const Header = () => {
+    return (
+        <header className="header">
+            <div className="container">
+                <h1>Pedro Joestar</h1>
+                <nav className="nav">
+                    <ul className="nav-links">
+                        <li><Link to="/">Domů</Link></li>
+                        <li><Link to="/About">O mně</Link></li>
+                        <li><Link to="/Projekty">Projekty</Link></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    );
+}
 
-    export default Header;
+export default Header;
